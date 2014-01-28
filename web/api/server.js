8e609e32aad303643f1b4a3cd0b70bc13177ca9e
Evgeny Zhivitsa <J.Zhivitsa@gmail.com>
2014-01-28 16:06:38 +0300

    added processing of the get and post queries

--- web/api/server.js
+++ web/api/server.js
@@ -5,6 +5,16 @@ function startServer (route, handle) {
 	http.createServer(function (request, response) {
 
 		var pathname = url.parse(request.url).pathname;
+		if ( request.method.toLowerCase() === 'get' ) {
+			console.log('get request');
+			processGetRequest(pathname, url, route, handle, request, response);
+		}
+		else if ( request.method.toLowerCase() === 'post' ) {
+			console.log('post request');
+			processPostRequest(pathname, route, handle, request, response);
+		}
+
+		
     	console.log("Request for " + pathname + " received.");
     	route(handle, pathname);
 		response.writeHead(200, {"Content-Type": "text/plain"});
@@ -14,6 +24,26 @@ function startServer (route, handle) {
 	}).listen('8888');
 
 	console.log('Server was started');
+}
+
+function processGetRequest (pathname, url, route, handle, request, response) {
+	var url_parts = url.parse(request.url, true),
+		query = url_parts.query;
+		
+	console.log(query);
+	route(handle, pathname, response);
+}
+
+function processPostRequest (pathname, route, handle, request, response) {
+	request.addListener("data", function(postDataChunk) {
+      postData += postDataChunk;
+      console.log("Received POST data chunk '"+
+      postDataChunk + "'.");
+    });
+
+    request.addListener("end", function() {
+      route(handle, pathname, response, postData);
+    });
 }
 
 exports.startServer = startServer;
