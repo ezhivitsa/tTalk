fcbe1851102ea6ffa9a62b8d438fe2c64a547acc
neymen <hiqopot@gmail.com>
2014-04-29 18:11:07 +0300

    Talk2
    Add all talk actions

--- web/content/js/talks/TalkController.js
+++ web/content/js/talks/TalkController.js
@@ -1,17 +1,14 @@
 app.controller('TalkCtrl', ['$scope', '$resource', '$routeParams', '$q', function ( $scope, $resource, $routeParams, $q ) {
 	
-	var talkService = $resource('../api/talk'),
-		commentsService = $resource('../api/comments'),
-		subscribeService = $resource('/api/subscribe'),
-		apiService = $resource('../api/:action',{
+	var apiService = $resource('../api/:action',{
 				action: '@action'
 			}),
-		pageCount = 0;
+		pageCount = { val: 1 };
 
 
 	function getTalk() {
 		var defer = $q.defer();
-		$scope.talk = talkService.get({ id: $routeParams.id },
+		$scope.talk = apiService.get({ action: 'talk', id: $routeParams.id },
 			function ( response ) {
 				defer.resolve();
 				$scope.talk.date = (new Date($scope.talk.date)).toLocaleString();
@@ -27,39 +24,93 @@ app.controller('TalkCtrl', ['$scope', '$resource', '$routeParams', '$q', functio
 	}
 
 	function getComments( page , size ) {
-		$scope.comments = commentsService.get({ id: $routeParams.id, page: page, page_size: size },
+		var comments = apiService.get({ action: 'comments', id: $routeParams.id, page: page.val, page_size: size },
 			function ( response ) {
-				console.log($scope.comments)
+				console.log(comments)
+				if (page.val != 1) {
+					$scope.comments = $scope.comments.concat(comments.comments);
+					$scope.isCommentsEnd = comments.isEnd;
+				} else {
+					$scope.comments = comments.comments;
+					$scope.isCommentsEnd = comments.isEnd;
+				}
+				page.val++;
 			},
 			function ( response ) {
-				defer.reject({
-					message: response.data.message,
-					status: response.status
-				});
 			});
-		return ++page;
 	}
 
 	getTalk();
-	pageCount = getComments(pageCount,10);
+	getComments(pageCount,10);
 
 	$scope.subscribe = function ( event ) {
 		event.preventDefault();
+		var defer = $q.defer();
 		if (!$scope.talk.isCanSubscribe) {
 			return;
 		}
-		$scope.talk.participants = apiService.save({ action: 'subscribe' },{ id: $scope.talk._id },function(response) {
-				console.log($scope.talk.participants);
+		var participants = apiService.save({ action: 'subscribe' },{ id: $routeParams.id },function(response) {
+				defer.resolve();
+				$scope.talk.isCanSubscribe = false;
+				$scope.talk.participants = participants;
+				console.log(participants);
 			},function(response) {
 				defer.reject({
 					message: response.data.message,
 					status: response.status
 				});
 			});
+		return defer.promise;
 	};
 
+	$scope.eval = function ( event, mark ) {
+		event.preventDefault();
+		var defer = $q.defer();
+		if (!$scope.talk.isCanEvaluate) {
+			return;
+		}
+		var rating = apiService.save({ action: 'evaluatetalk' },{ id: $routeParams.id, mark: mark },function(response) {
+				defer.resolve();
+				$scope.talk.isCanEvaluate = false;
+				$scope.talk.rating = rating.rating;
+				console.log(rating);
+			},function(response) {
+				defer.reject({
+					message: response.data.message,
+					status: response.status
+				});
+			});
+		return defer.promise;
+	}
+
+	$scope.moreComments = function ( event ) {
+		event.preventDefault();
+		if($scope.comments.isEnd) {
+			return;
+		}
+		getComments(pageCount,10);
+	}
+
+	$scope.createComment = function ( event ) {
+		event.preventDefault();
+		var defer = $q.defer();
+		apiService.save({ action: 'comment' },{ id: $routeParams.id, text: $scope.talk.newComment },function(response) {
+				defer.resolve();
+				$scope.talk.newComment = '';
+				pageCount.val = 1;
+				getComments(pageCount,10);
+			},function(response) {
+				defer.reject({
+					message: response.data.message,
+					status: response.status
+				});
+			});
+		return defer.promise;
+	}
+
 }]);
-app.directive('comment', [function(){
+
+app.directive('comment', [ '$resource', '$q', function( $resource, $q ){
 	return {
 		scope: {
 			data: '=data'
@@ -71,11 +122,32 @@ app.directive('comment', [function(){
 					'<div class="bottom">' +
 						'<span class="rating">{{data.rating}}</span>' +
 						'<span class="cEval" ng-if="data.isCanEvaluate">' +
-							'<a href="../talk/like" class="evalButton like" ng-cick="cLike($event)">+</a>' +
-							'<a href="../talk/dislike" class="evalButton dislike" ng-cick="cDislike($event)">-</a>' +
+							'<a href="#like" class="evalButton like" ng-click="cEval($event,1)">+</a>' +
+							'<a href="#dislike" class="evalButton dislike" ng-click="cEval($event,-1)">-</a>' +
 						'</span>' +
 					'</div>' +
 				'</div>',
-		replace: true
+		replace: true,
+		link: function( $scope, iElm, iAttrs, controller ) {
+			var evalTalkService = $resource('/api/evaluatecomment');
+			$scope.cEval = function ( event, mark ) {
+				event.preventDefault();
+				var defer = $q.defer();
+				if (!$scope.data.isCanEvaluate) {
+					return;
+				}
+				var rating = evalTalkService .save({},{ id: $scope.data._id, mark: mark },function(response) {
+						defer.resolve();
+						$scope.data.isCanEvaluate = false;
+						$scope.data.rating = rating.rating;
+					},function(response) {
+						defer.reject({
+							message: response.data.message,
+							status: response.status
+						});
+					});
+				return defer.promise;
+			}
+		}
 	};
 }]);
